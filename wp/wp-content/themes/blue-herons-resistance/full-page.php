2400c3003aedc433c24b58559c036466bf724900
John Luetke <john@johnluetke.net>
2015-01-14 11:10:14 -0800

    Adding subtheme

---
+++ wp/wp-content/themes/blue-herons-resistance/full-page.php
@@ -0,0 +1,56 @@
+<?php
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Full Page Template
+ *
+Template Name:  BHR Full Page (no menu or sidebar)
+ */
+
+get_header(); ?>
+
+<?php if (function_exists('adrotate_ad')) { echo adrotate_ad(1); } ?>
+
+<div id="content-full" class="grid col-940">
+
+	<?php if( have_posts() ) : ?>
+
+		<?php while( have_posts() ) : the_post(); ?>
+
+			<?php responsive_entry_before(); ?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php responsive_entry_top(); ?>
+
+				<h1 class="post-title"><?php the_title(); ?></h1>
+
+				<div class="post-entry">
+					<?php the_content( __( 'Read more &#8250;', 'responsive' ) ); ?>
+					<?php wp_link_pages( array( 'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ), 'after' => '</div>' ) ); ?>
+				</div>
+				<!-- end of .post-entry -->
+
+				<?php get_template_part( 'post-data' ); ?>
+
+				<?php responsive_entry_bottom(); ?>
+			</div><!-- end of #post-<?php the_ID(); ?> -->
+			<?php responsive_entry_after(); ?>
+
+		<?php
+		endwhile;
+
+		get_template_part( 'loop-nav' );
+
+	else :
+
+		get_template_part( 'loop-no-posts' );
+
+	endif;
+	?>
+
+</div><!-- end of #content-full -->
+
+<?php get_footer(); ?>
